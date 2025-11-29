9fb6f34902aa8db7229264fc853e64afb4d4def8
ElFalch <eleanorfalch95@gmail.com>
2025-11-29 11:40:57 +0000

    fix: remove unused code from views.py

--- booking/views.py
+++ booking/views.py
@@ -2,43 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from datetime import datetime, timedelta
 from .models import Appointment
 from django.views.generic import CreateView
-from django.contrib import messages
 
-def validWeekday(days):
-    #Loop days you want in the next 21 days:
-    today = datetime.now()
-    weekdays = []
-    for i in range (0, days):
-        x = today + timedelta(days=i)
-        y = x.strftime('%A')
-        if y == 'Monday' or y == 'Saturday' or y == 'Wednesday':
-            weekdays.append(x.strftime('%Y-%m-%d'))
-    return weekdays
-
-def isWeekdayValid(x):
-    validateWeekdays = []
-    for j in x:
-        if Appointment.objects.filter(day=j).count() < 10:
-            validateWeekdays.append(j)
-    return validateWeekdays
 
 def booking(request):
-    weekdays = validWeekday(200)
+    """ A view to return the index page """
 
-    #Only show the days that are not full:
-    validateWeekdays = isWeekdayValid(weekdays)
+    return render(booking, 'booking/appointment_form.html')
 
-    if request.method == 'POST':
-        day = request.POST.get('day')
-        request.session['day'] = day
-
-    return render(request,'booking/booking.html', {
-            'weekdays':weekdays,
-            'validateWeekdays':validateWeekdays,
-        })
 
 class BookingCreateView(CreateView):
     model = Appointment
