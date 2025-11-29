006ff355c82f0f214ea849d91d187471657be305
ElFalch <eleanorfalch95@gmail.com>
2025-11-29 11:39:38 +0000

    feat: add Date_Choices tuple for appointment form day dropdown

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime
+from datetime import datetime, timedelta 
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -16,10 +16,21 @@ TIME_CHOICES = (
     ("7:30 PM", "7:30 PM"),
 )
 
+
+DATE_CHOICES = (
+    (str(datetime.now() + timedelta(1)), str(datetime.now() + timedelta(1))),
+    (str(datetime.now() + timedelta(2)), str(datetime.now() + timedelta(2))),
+    (str(datetime.now() + timedelta(3)), str(datetime.now() + timedelta(3))),
+    (str(datetime.now() + timedelta(4)), str(datetime.now() + timedelta(4))),
+    (str(datetime.now() + timedelta(5)), str(datetime.now() + timedelta(5))),
+)
+
+
 class Appointment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    day = models.DateField(default=datetime.now)
-    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="3 PM")
+    day = models.CharField(max_length=26, choices=DATE_CHOICES, default= datetime.now() + timedelta(1))
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default= "3 PM")
     time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+    
     def __str__(self):
         return f"{self.user.username} | day: {self.day} | time: {self.time}"
