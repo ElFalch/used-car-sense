c3b64d53a30e1caf71340e7e1a458c27cbfb3d0f
ElFalch <eleanorfalch95@gmail.com>
2025-11-30 17:23:49 +0000

    feat: add checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+def checkout(request):
+    appointment = request.session.get('appointment', {})
+    if not appointment:
+        messages.error(request, "You haven't chosen an appointment")
+        return redirect(reverse('booking'))
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
