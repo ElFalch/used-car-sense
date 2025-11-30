54060a5569db959708b4a96ea68d6d1c15661688
ElFalch <eleanorfalch95@gmail.com>
2025-11-30 15:39:39 +0000

    fix: change model and variable names in  checkout admin to reflect those in models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,26 +1,25 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem
+from .models import Order, OrderDetails
 
 
 class OrderDetailsAdminInline(admin.TabularInline):
-    model = OrderLineItem
-    readonly_fields = ('lineitem_total',)
+    model = OrderDetails
 
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderDetailsAdminInline,)
 
-    readonly_fields = ('order_number', 'date',
+    readonly_fields = ('date_ordered',
                        'grand_total',)
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('appointment', 'date_ordered', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county','grand_total',)
+              'street_address2', 'county', 'grand_total',)
 
-    list_display = ('order_number', 'date', 'full_name',
+    list_display = ('date_ordered', 'appointment', 'full_name',
                     'grand_total',)
 
-    ordering = ('-date',)
+    ordering = ('-date_ordered',)
 
 admin.site.register(Order, OrderAdmin)
