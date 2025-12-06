d859b34c85f8053d008b4c1f6d16f6a0f9df9769
ElFalch <eleanorfalch95@gmail.com>
2025-12-06 14:13:52 +0000

    feat: add day and time to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -3,15 +3,18 @@ from django.contrib import messages
 from .forms import OrderForm
 
 def checkout(request):
-    appointment = request.session.get('appointment', {})
-    if not appointment:
+    day = request.session.get('day', {});
+    time = request.session.get('time', {});
+    if not day:
         messages.error(request, "You haven't chosen an appointment")
         return redirect(reverse('booking'))
-    
+    print(day)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'day': day,
+        'time': time,
     }
 
     return render(request, template, context)
