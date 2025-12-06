764b15d2acf7dca4da0a4e896e06d235bbe0307b
ElFalch <eleanorfalch95@gmail.com>
2025-12-06 18:05:06 +0000

    feat: add stripe secret and public keys to checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -4,6 +4,13 @@ from django.conf import settings
 
 from .forms import OrderForm
 
+import stripe
+
+def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
 def checkout(request):
     day = request.session.get('day', {});
     time = request.session.get('time', {});
@@ -12,7 +19,14 @@ def checkout(request):
         return redirect(reverse('booking'))
 
     stripe_total = 20
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
+    print(intent)
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
