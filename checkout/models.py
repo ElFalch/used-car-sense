cb0cebaf78405da5d4066fb8d088c29d30e0d2d7
ElFalch <eleanorfalch95@gmail.com>
2026-03-01 16:55:43 +0000

    feat: add functions to generate order number

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 from booking.models import Appointment
@@ -17,7 +19,24 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
 
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+    
 class OrderDetails(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='Orderdetails')
     appointment = models.ForeignKey(Appointment, null=False, blank=False, on_delete=models.CASCADE)
