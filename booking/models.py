43b17b72b02d526d5c6af19764fd18d3928ae046
ElFalch <eleanorfalch95@gmail.com>
2026-05-02 18:52:03 +0100

    fix: add imports back in to models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+
 class TimeSlot(models.Model):
     day = models.DateField()
     time = models.CharField(max_length=10)
@@ -21,4 +24,4 @@ class Appointment(models.Model):
     time_ordered = models.DateTimeField(default=datetime.now, blank=True)
     
     def __str__(self):
-        return f"{self.user{self.timeslot.day} | {self.timeslot.time}"
+        return f"{self.user} | {self.timeslot.day} | {self.timeslot.time}"
