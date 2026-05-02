65029ebbe00e3f7dada95dfe4738f9a01b31216a
ElFalch <eleanorfalch95@gmail.com>
2026-05-02 13:27:36 +0100

    fix: add uniqueness protection to booking model

--- booking/models.py
+++ booking/models.py
@@ -28,9 +28,14 @@ DATE_CHOICES = (
 
 class Appointment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    day = models.CharField(max_length=26, choices=DATE_CHOICES, default= datetime.now() + timedelta(1))
-    time = models.CharField(max_length=10, choices=TIME_CHOICES, default= "3 PM")
+    day = models.CharField(max_length=26, choices=DATE_CHOICES)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES)
     time_ordered = models.DateTimeField(default=datetime.now, blank=True)
     
     def __str__(self):
         return f"{self.user.username} | day: {self.day} | time: {self.time}"
+ # Uniqueness protection taken from: https://stackoverflow.com/questions/57149015/creating-a-models-uniqueconstraint-in-abstract-model   
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['day', 'time'], name='unique_day_time')
+        ]
