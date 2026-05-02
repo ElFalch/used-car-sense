c693af77cc111a8c9236deb299b1c9689b048be5
ElFalch <eleanorfalch95@gmail.com>
2026-05-02 19:02:00 +0100

    fix: make new url patterns match booking create view

--- booking/urls.py
+++ booking/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path 
 from . import views
 
-
 urlpatterns = [
     path('', views.BookingCreateView.as_view(), name='booking'),
-    path("my-bookings/", MyBookingsView.as_view(), name="my_bookings"),
-    path("edit/<int:pk>/", BookingUpdateView.as_view(), name="edit_booking"),
-    path("delete/<int:pk>/", BookingDeleteView.as_view(), name="delete_booking"),
+    path('', views.MyBookingsView.as_view(), name="my_bookings"),
+    path('', views.BookingUpdateView.as_view(), name="edit_booking"),
+    path('', views.BookingDeleteView.as_view(), name="delete_booking"),
 ]
