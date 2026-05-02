4731166675a9e832113ce2058eba25f8273cf2bb
ElFalch <eleanorfalch95@gmail.com>
2026-05-02 14:46:38 +0100

    fix: add allauth middleware to settings.py

--- used_car_sense/settings.py
+++ used_car_sense/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'used_car_sense.urls'
