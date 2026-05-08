7f9499515743acaf9066b6addfda3a0d3b0844e7
ElFalch <eleanorfalch95@gmail.com>
2026-05-08 19:29:52 +0100

    fix: change default from email to @tpeelscustomwheels.com

--- used_car_sense/settings.py
+++ used_car_sense/settings.py
@@ -175,7 +175,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'tpeelscustomwheels@example.com'
+    DEFAULT_FROM_EMAIL = 'example@tpeelscustomwheels.com'
 
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
