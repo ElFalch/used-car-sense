a2b1466dc86b87ac42651f049f6c49b6586de317
ElFalch <eleanorfalch95@gmail.com>
2026-05-08 16:38:00 +0100

    fix: give each booking url a different path

--- booking/urls.py
+++ booking/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BookingCreateView.as_view(), name='booking'),
-    path('', views.MyBookingsView.as_view(), name="my_bookings"),
-    path('', views.BookingUpdateView.as_view(), name="edit_booking"),
-    path('', views.BookingDeleteView.as_view(), name="delete_booking"),
+    path('book/', views.BookingCreateView.as_view(), name='booking'),
+    path('my-bookings/', views.MyBookingsView.as_view(), name="my_bookings"),
+    path('edit/<int:pk>/', views.BookingUpdateView.as_view(), name="edit_booking"),
+    path('delete/<int:pk>/', views.BookingDeleteView.as_view(), name="delete_booking"),
 ]
