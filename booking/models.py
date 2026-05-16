a333767ff1fe6d55a8a7602a31decaaa2f8808b3
ElFalch <eleanorfalch95@gmail.com>
2026-05-16 15:09:13 +0100

    feat: add status field to appointment model

--- booking/models.py
+++ booking/models.py
@@ -19,6 +19,14 @@ class Appointment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     timeslot = models.OneToOneField(TimeSlot, on_delete=models.CASCADE)
     time_ordered = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("pending", "Pending Payment"),
+            ("confirmed", "Confirmed"),
+        ],
+        default="pending"
+    )
     
     def __str__(self):
         return f"{self.user} | {self.timeslot}"
