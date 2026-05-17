43efe60c3492759a0aaa318a59c9176b78ec11c2
ElFalch <eleanorfalch95@gmail.com>
2026-05-17 15:50:06 +0100

    fix: register appointment model with admin

--- booking/admin.py
+++ booking/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import TimeSlot
-admin.site.register(TimeSlot)
+from .models import Appointment
 
+admin.site.register(TimeSlot)
+admin.site.register(Appointment)
