b2f10dbbf610fb7836c6dc58fa1ddba07d309d89
ElFalch <eleanorfalch95@gmail.com>
2026-05-19 12:23:18 +0100

    fix: correct formatting of booking urls

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('book/', views.BookingCreateView.as_view(), name='booking'),
     path('my-bookings/', views.MyBookingsView.as_view(), name="my_bookings"),
-    path('edit/<int:pk>/', views.BookingUpdateView.as_view(), name="edit_booking"),
-    path('delete/<int:pk>/', views.BookingDeleteView.as_view(), name="delete_booking"),
+    path(
+        'edit/<int:pk>/',
+        views.BookingUpdateView.as_view(),
+        name="edit_booking"
+    ),
+    path(
+        'delete/<int:pk>/',
+        views.BookingDeleteView.as_view(),
+        name="delete_booking"),
 ]
