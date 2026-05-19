3f14a5f881c8963ad8397c779877a5aaee2e7ae7
ElFalch <eleanorfalch95@gmail.com>
2026-05-19 12:41:22 +0100

    fix: shorten lines in checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -6,7 +6,12 @@ from booking.models import Appointment
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    appointment = models.OneToOneField(Appointment, null=True, blank=True, on_delete=models.SET_NULL)
+    appointment = models.OneToOneField(
+        Appointment,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -17,7 +22,12 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
 
     def _generate_order_number(self):
         """
